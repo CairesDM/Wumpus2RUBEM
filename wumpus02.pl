c9245f28308aa7a337b3e7b5b18e4c21bdbdefb5
Douglas Medeiros Caires <douglasmcaires@hotmail.com>
2013-11-21 17:17:18 -0300

    adicionando funcao para atualizar a posicao

--- wumpus02.pl
+++ wumpus02.pl
@@ -20,14 +20,19 @@ atualizar_orientacao_direita :- orientacao(X), retractall(orientacao(_)), Y is X
 atualizar_orientacao_esquerda :- orientacao(270), retractall(orientacao(_)), Y is 0, assert(orientacao(Y)).
 atualizar_orientacao_esquerda :- orientacao(X), retractall(orientacao(_)), Y is X+90, assert(orientacao(Y)).
 
+atualizar_posicao:-orientacao(0), posicao_atual(X,Y), retractall(posicao_atual(_)), Z is X+1, W is Y, assert(posicao_atual(Z,W).
+atualizar_posicao:-orientacao(90), posicao_atual(X,Y),retractall(posicao_atual(_)), Z is X, W is Y+1, assert(posicao_atual(Z,W).
+atualizar_posicao:-orientacao(180),posicao_atual(X,Y),retractall(posicao_atual(_)), Z is X-1 , W is Y, assert(posicao_atual(Z,W).
+atualizar_posicao:-orientacao(270),posicao_atual(X,Y),retractall(posicao_atual(_)), Z is X, W is Y-1, assert(posicao_atual(Z,W).
+
 dobrarEsquerda(turnleft) :- viradas(0), somar(0).
-dobrarEsquerda(goforward):- viradas(1), atualizar_orientacao_esquerda, zerar.
+dobrarEsquerda(goforward):- viradas(1), zerar, atualizar_orientacao_esquerda, atualizar_posicao.
  
 dobrarDireita(turnright) :- viradas(0), somar(0).
-dobrarDireita(goforward) :- viradas(1), atualizar_orientacao_direita, zerar.
+dobrarDireita(goforward) :- viradas(1), zerar, atualizar_orientacao_direita, atualizar_posicao.
 
 atras(turnleft):- viradas(0), somar(0).
-atras(turnleft):- viradas(1), atualizar_orientacao_esquerda, atualizar_orientacao_esquerda, zerar.
+atras(turnleft):- viradas(1), zerar, atualizar_orientacao_esquerda, atualizar_orientacao_esquerda, atualizar_posicao.
 
 somar(X):-
     retractall(viradas(_)),
@@ -36,3 +41,4 @@ somar(X):-
 zerar:-
     retractall(viradas(_)),
     assert(viradas(0)).
+
