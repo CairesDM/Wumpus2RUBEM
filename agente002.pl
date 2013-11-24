297a7d12848000930f21891a67ec53db8885e4dc
Douglas Medeiros Caires <douglasmcaires@hotmail.com>
2013-11-23 23:37:22 -0300

    adicionando funca para consertar bug da batida

--- agente002.pl
+++ agente002.pl
@@ -3,7 +3,7 @@
 :-dynamic([orientacao/1, viradas/1, posicao_atual/2, casas_visitadas/3]).
 
 init_agent:-
-    writeln("Comecando teste"),
+    writeln('Comecando teste'),
         assert(orientacao(0)),
         assert(viradas(0)),
         assert(posica_atual(1,1)),
@@ -16,6 +16,8 @@ run_agent(Pc,Ac):-
     writeln(Pc),
     agente002(Pc,Ac).
 
+agente002([_,_,_,yes,_],Ac):-  ajuste_posicao(Ac).
+
 atualizar_orientacao_direita :- orientacao(0), retractall(orientacao(_)), Y is 270, assert(orientacao(Y)).
 atualizar_orientacao_direita :- orientacao(X), retractall(orientacao(_)), Y is X-90, assert(orientacao(Y)).
 
@@ -25,10 +27,15 @@ atualizar_orientacao_esquerda :- orientacao(X), retractall(orientacao(_)), Y is 
 atualizar_casas_visitadas(A,B,O):- casas_visitadas(A,B,_).%caso a casa ja tenha sido visitada, a orientacao antiga se mantem.
 atualizar_casas_visitadas(A,B,O):- assert(casas_visitadas(A,B,O)).%caso nao tenha sido visitada, atualiza de onde ele veio.
 
-atualizar_posicao:-orientacao(0), posicao_atual(X,Y), retractall(posicao_atual(_)), Z is X+1, W is Y, assert(posicao_atual(Z,W),atualizar_casas_visitadas(Z,W,0).
-atualizar_posicao:-orientacao(90), posicao_atual(X,Y),retractall(posicao_atual(_)), Z is X, W is Y+1, assert(posicao_atual(Z,W),atualizar_casas_visitadas(Z,W,90).
-atualizar_posicao:-orientacao(180),posicao_atual(X,Y),retractall(posicao_atual(_)), Z is X-1 , W is Y, assert(posicao_atual(Z,W),atualizar_casas_visitadas(Z,W,180).
-atualizar_posicao:-orientacao(270),posicao_atual(X,Y),retractall(posicao_atual(_)), Z is X, W is Y-1, assert(posicao_atual(Z,W),atualizar_casas_visitadas(Z,W,270).
+atualizar_posicao:-orientacao(0), posicao_atual(X,Y), retractall(posicao_atual(_)), Z is X+1, W is Y, assert(posicao_atual(Z,W)),atualizar_casas_visitadas(Z,W,0).
+atualizar_posicao:-orientacao(90), posicao_atual(X,Y),retractall(posicao_atual(_)), Z is X, W is Y+1, assert(posicao_atual(Z,W)),atualizar_casas_visitadas(Z,W,90).
+atualizar_posicao:-orientacao(180),posicao_atual(X,Y),retractall(posicao_atual(_)), Z is X-1 , W is Y,assert(posicao_atual(Z,W)),atualizar_casas_visitadas(Z,W,180).
+atualizar_posicao:-orientacao(270),posicao_atual(X,Y),retractall(posicao_atual(_)), Z is X, W is Y-1, assert(posicao_atual(Z,W)),atualizar_casas_visitadas(Z,W,270).
+
+ajuste_posicao(turnleft):-  orientacao(0), posicao_atual(X,Y), retractall(posicao_atual(_,_)), Z is X-1,  assert(posicao_atual(Z,Y)),atualizar_orientacao_esquerda.
+ajuste_posicao(turnright):-  orientacao(90), posicao_atual(X,Y), retractall(posicao_atual(_,_)), W is Y-1, assert(posicao_atual(X,W)),atualizar_orientacao_direita.
+ajuste_posicao(turnright):-  orientacao(180), posicao_atual(X,Y), retractall(posicao_atual(_,_)), Z is X+1,assert(posicao_atual(Z,Y)),atualizar_orientacao_direita.
+ajuste_posicao(turnleft):-  orientacao(270), posicao_atual(X,Y), retractall(posicao_atual(_,_)), W is Y+1, assert(posicao_atual(X,W)),atualizar_orientacao_esquerda.
 
 dobrarEsquerda(turnleft) :- viradas(0), somar(0).
 dobrarEsquerda(goforward):- viradas(1), zerar, atualizar_orientacao_esquerda, atualizar_posicao.
@@ -37,7 +44,7 @@ dobrarDireita(turnright) :- viradas(0), somar(0).
 dobrarDireita(goforward) :- viradas(1), zerar, atualizar_orientacao_direita, atualizar_posicao.
 
 atras(turnleft):- viradas(0), somar(0).
-atras(turnleft):- viradas(1),somar(1),
+atras(turnleft):- viradas(1),somar(1).
 atras(turnleft):- viradas(2), zerar, atualizar_orientacao_esquerda, atualizar_orientacao_esquerda, atualizar_posicao.
 
 frente(goforward):- atualizar_posicao.
